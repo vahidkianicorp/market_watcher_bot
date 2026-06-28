b007174fbaf650f08c166c6a353375ec30c63ea9
vahidkianicorp <vahidkiani@proton.me>
2026-06-28 10:59:25 +0330

    logger
    setting up logger using logging lib

--- bot.py
+++ bot.py
@@ -7,3 +7,9 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 # Load environment variables from .env file
 load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
